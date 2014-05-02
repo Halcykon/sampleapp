fa3f1c9245f5727af03da407ab93f195990964ea
Frank Teng <frank.t.teng@gmail.com>
2014-05-02 16:42:53 -0400

    Created my own pages. I just need to reinstall sass rails

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,24 +4,53 @@ describe "StaticPages" do
 
 	describe "Home page" do
 
-		it "should have the content 'Sample App'" do
+		it "should have the title 'Welcome'" do
+			visit 'static_pages/home'
+			expect(page).to have_title('Welcome')
+		end
+
+		it "should have the content 'Welcome'" do
 			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
+			expect(page).to have_content('Welcome')
 		end
 	end
 
-	describe "Help page" do
+	describe "Photography" do
 
-		it "should have the content 'help me..'" do
-	  		visit '/static_pages/help'
-  			expect(page).to have_content('help me..')
+		it "should have the title 'Photography'" do
+			visit 'static_pages/photographs'
+			expect(page).to have_title('Photography')
+
+		it "should have the content 'Photographs'" do
+	  		visit '/static_pages/photographs'
+  			expect(page).to have_content('Photographs')
 		end
 	end
 
 	describe "About page" do
+
+		it "should have the title 'About'" do
+			visit 'static_pages/about'
+			expect(page).to have_title('About')
+		end
+
 		it "should have the content 'About'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About')
 		end
 	end
+
+	describe "Contact" do
+
+		it "should have the title 'Contact'" do
+			visit 'static_pages/contact'
+			expect(page).to have_title('Contact')
+		end
+
+		it "should have the content 'contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('contact')
+		end
+	end	
+
 end	
